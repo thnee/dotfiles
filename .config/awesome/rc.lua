3df97f6943f5a2e713e5b69255e5b578e8c781cf
Mattias Lindvall <mattias.lindvall@gmail.com>
2015-05-11 13:55:19 +0200

    use gmrun instead of promptbox

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -244,7 +244,8 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
+    -- awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
+    awful.key({ modkey },            "r",     function () awful.util.spawn("gmrun") end),
 
     awful.key({ modkey }, "x",
               function ()
